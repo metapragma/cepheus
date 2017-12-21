606c33574610e2474b18f28f47e89cca0d8d4c98
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-21 15:13:12 +0400

    add: tachyons opacity

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,11 @@
-import Vue from 'vue';
-import Router from 'vue-router';
-import Hello from '../components/Hello.vue';
-import PartialsSkins from '../components/partials-skins.vue';
-import PartialsTextDecoration from '../components/partials-text-decoration.vue';
+import Vue from 'vue'
+import Router from 'vue-router'
+import Hello from '../components/Hello.vue'
+import PartialsSkins from '../components/partials-skins.vue'
+import PartialsTextDecoration from '../components/partials-text-decoration.vue'
+import PartialsOpacity from '../components/partials-opacity.vue'
 
-Vue.use(Router);
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
@@ -22,7 +23,11 @@ export default new Router({
       path: '/partials/text-decoration',
       component: PartialsTextDecoration
     },
+    {
+      path: '/partials/opacity',
+      component: PartialsOpacity
+    },
   ],
-});
+})
 
 // TODO: add a 404 router
