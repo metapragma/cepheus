19f717c5164d9b026ab658cefd855643a16cc630
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-21 15:13:12 +0400

    add: tachyons opacity

---
+++ src/components/partials-opacity.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="tests">
+    <div id="op100" class="box o-100"></div>
+    <div id="op90" class="box o-90"></div>
+    <div id="op80" class="box o-80"></div>
+    <div id="op70" class="box o-70"></div>
+    <div id="op60" class="box o-60"></div>
+    <div id="op50" class="box o-50"></div>
+    <div id="op40" class="box o-40"></div>
+    <div id="op30" class="box o-30"></div>
+    <div id="op20" class="box o-20"></div>
+    <div id="op10" class="box o-10"></div>
+    <div id="op05" class="box o-05"></div>
+    <div id="op025" class="box o-025"></div>
+    <div id="op0" class="box o-0"></div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+
+.box {
+  margin-left: 6em;
+  margin-bottom: 6em;
+  height: 5em; 
+  width: 5em; 
+  background-color: magenta;
+}
+</style>
--- test/end-to-end/specs/test.js
+++ test/end-to-end/specs/test.js
@@ -26,9 +26,6 @@ module.exports = {
       .end()
   },
   'partials/text-decoration': function (browser) {
-    // automatically uses dev Server port from /config.index.js
-    // default: http://localhost:8080
-    // see nightwatch.conf.js
     const devServer = browser.globals.devServerURL
 
     browser
@@ -37,6 +34,29 @@ module.exports = {
       .assert.cssProperty(".strike", "text-decoration", "line-through solid rgb(0, 0, 0)")
       .assert.cssProperty(".underline", "text-decoration", "underline solid rgb(0, 0, 0)")
       .assert.cssProperty(".no-underline", "text-decoration", "none solid rgb(0, 0, 0)")
+      .end()
+  },
+  'partials/opacity': function (browser) {
+    // automatically uses dev Server port from /config.index.js
+    // default: http://localhost:8080
+    // see nightwatch.conf.js
+    const devServer = browser.globals.devServerURL
+
+    browser
+      .url(`${devServer}/partials/opacity`)
+      .waitForElementVisible('#tests', 5000)
+      .assert.cssProperty("#op100", "opacity", "1")
+      .assert.cssProperty("#op90", "opacity", "0.9")
+      .assert.cssProperty("#op70", "opacity", "0.7")
+      .assert.cssProperty("#op60", "opacity", "0.6")
+      .assert.cssProperty("#op50", "opacity", "0.5")
+      .assert.cssProperty("#op40", "opacity", "0.4")
+      .assert.cssProperty("#op30", "opacity", "0.3")
+      .assert.cssProperty("#op20", "opacity", "0.2")
+      .assert.cssProperty("#op10", "opacity", "0.1")
+      .assert.cssProperty("#op05", "opacity", "0.05")
+      .assert.cssProperty("#op025", "opacity", "0.025")
+      .assert.cssProperty("#op0", "opacity", "0")
       .end()
   }
 }
