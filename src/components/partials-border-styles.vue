895f09bbfbbfe48f4666a19ae25d897794e21ce6
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-21 21:43:00 +0400

    add: borders, border-styles, border-widths, border-radius

---
+++ src/components/partials-border-styles.vue
@@ -0,0 +1,14 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ba b--dotted" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l2" class="ba b--dashed" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l3" class="ba b--solid" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l4" class="ba b--none" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
---
+++ src/components/partials-border-widths.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ba bw0" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l2" class="ba bw1" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l3" class="ba bw2" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l4" class="ba bw3" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l5" class="ba bw4" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l6" class="ba bw5" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l7" class="ba bt-0" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l8" class="ba br-0" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l9" class="ba bb-0" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l10" class="ba bl-0" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
