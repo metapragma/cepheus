8096b4b9c8f38bbb89ae35460f5f00f9c02f8a29
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-22 18:16:17 +0400

    fix: remove coloring

--- src/components/partials-letter-spacing.vue
+++ src/components/partials-letter-spacing.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="tests" style="margin: 5em;">
     <div id="case-one">
-      <div id="l1" class="bg-navy tracked" style="width: 30rem; height: 30rem; padding: 1em;">
+      <div id="l1" class="ba tracked" style="width: 30rem; height: 30rem; padding: 1em;">
         Lorem Ipsum
-        <div id="l2" class="bg-dark-blue tracked-tight" style="width: 20rem; height: 20rem; padding: 1em;">
+        <div id="l2" class="ba tracked-tight" style="width: 20rem; height: 20rem; padding: 1em;">
           Lorem Ipsum
-          <div id="l3" class="bg-blue tracked-mega" style="width: 15rem; height: 15rem; padding: 1em;">
+          <div id="l3" class="ba tracked-mega" style="width: 15rem; height: 15rem; padding: 1em;">
             Lorem Ipsum
-            <div id="l4" class="bg-light-blue" style="width: 10rem; height: 10rem; padding: 1em;">
+            <div id="l4" class="ba" style="width: 10rem; height: 10rem; padding: 1em;">
               Lorem Ipsum
-              <div id="l5" class="bg-lightest-blue" style="width: 5rem; height: 5rem; padding: 1em;">
+              <div id="l5" class="ba" style="width: 5rem; height: 5rem; padding: 1em;">
                 Lorem Ipsum
               </div>
             </div>
--- src/components/partials-opacity.vue
+++ src/components/partials-opacity.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="tests">
-    <div id="op100" class="box o-100"></div>
-    <div id="op90" class="box o-90"></div>
-    <div id="op80" class="box o-80"></div>
-    <div id="op70" class="box o-70"></div>
-    <div id="op60" class="box o-60"></div>
-    <div id="op50" class="box o-50"></div>
-    <div id="op40" class="box o-40"></div>
-    <div id="op30" class="box o-30"></div>
-    <div id="op20" class="box o-20"></div>
-    <div id="op10" class="box o-10"></div>
-    <div id="op05" class="box o-05"></div>
-    <div id="op025" class="box o-025"></div>
-    <div id="op0" class="box o-0"></div>
+    <div id="op100" class="box o-100 bg-black"></div>
+    <div id="op90" class="box o-90 bg-black"></div>
+    <div id="op80" class="box o-80 bg-black"></div>
+    <div id="op70" class="box o-70 bg-black"></div>
+    <div id="op60" class="box o-60 bg-black"></div>
+    <div id="op50" class="box o-50 bg-black"></div>
+    <div id="op40" class="box o-40 bg-black"></div>
+    <div id="op30" class="box o-30 bg-black"></div>
+    <div id="op20" class="box o-20 bg-black"></div>
+    <div id="op10" class="box o-10 bg-black"></div>
+    <div id="op05" class="box o-05 bg-black"></div>
+    <div id="op025" class="box o-025 bg-black"></div>
+    <div id="op0" class="box o-0 bg-black"></div>
   </div>
 </template>
 
@@ -24,6 +24,5 @@
   margin-bottom: 6em;
   height: 5em; 
   width: 5em; 
-  background-color: magenta;
 }
 </style>
