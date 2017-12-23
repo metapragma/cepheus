d4c9274691c9646159ffe1cd4acd152d855f8e02
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-23 18:00:51 +0400

    add: vertical-align

---
+++ src/components/partials-vertical-align.vue
@@ -0,0 +1,27 @@
+
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div style="margin: 4rem;">An 
+        <img id="l1" class="v-base" src="https://mdn.mozillademos.org/files/12245/frame_image.svg" alt="link" width="32" height="32"/> 
+        image with a baseline alignment.
+      </div>
+      <div style="margin: 4rem;">An 
+        <img id="l2" class="v-mid" src="https://mdn.mozillademos.org/files/12245/frame_image.svg" alt="link" width="32" height="32" /> 
+        image with a middle alignment.
+      </div>
+      <div style="margin: 4rem;">An 
+        <img id="l3" class="v-top" src="https://mdn.mozillademos.org/files/12245/frame_image.svg" alt="link" width="32" height="32" /> 
+        image with a top alignment.
+      </div>
+      <div style="margin: 4rem;">An 
+        <img id="l4" class="v-btm" src="https://mdn.mozillademos.org/files/12245/frame_image.svg" alt="link" width="32" height="32" /> 
+        image with a bottom alignment.
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -14,6 +14,7 @@ import PartialsBoxShadow from '../components/partials-box-shadow.vue'
 import PartialsZIndex from '../components/partials-z-index.vue'
 import PartialsWordBreak from '../components/partials-word-break.vue'
 import PartialsWhiteSpace from '../components/partials-white-space.vue'
+import PartialsVerticalAlign from '../components/partials-vertical-align.vue'
 
 Vue.use(Router)
 
@@ -76,6 +77,10 @@ export default new Router({
     {
       path: '/partials/white-space',
       component: PartialsWhiteSpace
+    },
+    {
+      path: '/partials/vertical-align',
+      component: PartialsVerticalAlign
     },
   ]
 })
