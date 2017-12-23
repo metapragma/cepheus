d468831a3974d8aee71c0266d86ed42af7b3a849
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-23 17:09:16 +0400

    add: white-space

---
+++ src/components/partials-white-space.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ws-normal" style="width: 10rem; height: 10rem; margin: 4em;">
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      </div>
+      <div id="l2" class="nowrap" style="width: 10rem; height: 10rem; margin: 4em;">
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      </div>
+      <div id="l3" class="pre" style="width: 10rem; height: 10rem; margin: 4em;">
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
