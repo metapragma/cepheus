bdd9fdc8356599f2d530d704c312b445d1ebfe64
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-23 19:00:34 +0400

    add: skins-pseudo

---
+++ src/components/partials-skins-pseudo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ba hover-pink" style="width: 15rem; height: 15rem; margin: 4em; padding: 1em;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste odio culpa quo quasi 
+        excepturi dolor recusandae laboriosam expedita deserunt incidunt, est aperiam facilis 
+        reprehenderit omnis laborum voluptate sapiente dicta libero!
+      </div>
+      <div id="l2" class="ba hover-dark-pink" style="width: 15rem; height: 15rem; margin: 4em; padding: 1em;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste odio culpa quo quasi 
+        excepturi dolor recusandae laboriosam expedita deserunt incidunt, est aperiam facilis 
+        reprehenderit omnis laborum voluptate sapiente dicta libero!
+      </div>
+      <div id="l3" class="ba hover-blue" style="width: 15rem; height: 15rem; margin: 4em; padding: 1em;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste odio culpa quo quasi 
+        excepturi dolor recusandae laboriosam expedita deserunt incidunt, est aperiam facilis 
+        reprehenderit omnis laborum voluptate sapiente dicta libero!
+      </div>
+      <div id="l4" class="ba hover-navy" style="width: 15rem; height: 15rem; margin: 4em; padding: 1em;">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste odio culpa quo quasi 
+        excepturi dolor recusandae laboriosam expedita deserunt incidunt, est aperiam facilis 
+        reprehenderit omnis laborum voluptate sapiente dicta libero!
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
