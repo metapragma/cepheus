1300d3d5fa07283355a20f452b044400ef270914
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-23 17:44:30 +0400

    add: white-space reset test

--- src/components/partials-white-space.vue
+++ src/components/partials-white-space.vue
@@ -8,8 +8,16 @@
       <div id="l2" class="nowrap" style="width: 10rem; height: 10rem; margin: 4em;">
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+        <div id="l4">
+          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          <div id="l5">
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+            Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
+          </div>
+        </div>
       </div>
-      <div id="l3" class="pre" style="width: 10rem; height: 10rem; margin: 4em;">
+      <div id="l3" class="pre" style="width: 10rem; height: 10rem; margin: 4em; margin-top: 14rem">
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
         Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum
       </div>
