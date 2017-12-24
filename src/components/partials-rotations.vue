f033460723c16cfb92703e599f78d21d7e1c25bf
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-24 13:46:44 +0400

    add: rotations

---
+++ src/components/partials-rotations.vue
@@ -0,0 +1,61 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ba rotate-45" style="width: 10rem; height: 10rem; margin: 4em;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Black_Arrow_Up.svg/2000px-Black_Arrow_Up.svg.png" 
+             style="width: 10rem; height: 10rem;" 
+        />
+      </div>
+      <div id="l2" class="ba rotate-90" style="width: 10rem; height: 10rem; margin: 4em;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Black_Arrow_Up.svg/2000px-Black_Arrow_Up.svg.png" 
+             style="width: 10rem; height: 10rem;" 
+        />
+      </div>
+      <div id="l3" class="ba rotate-135" style="width: 10rem; height: 10rem; margin: 4em;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Black_Arrow_Up.svg/2000px-Black_Arrow_Up.svg.png" 
+             style="width: 10rem; height: 10rem;" 
+        />
+      </div>
+      <div id="l4" class="ba rotate-180" style="width: 10rem; height: 10rem; margin: 4em;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Black_Arrow_Up.svg/2000px-Black_Arrow_Up.svg.png" 
+             style="width: 10rem; height: 10rem;" 
+        />
+      </div>
+      <div id="l5" class="ba rotate-225" style="width: 10rem; height: 10rem; margin: 4em;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Black_Arrow_Up.svg/2000px-Black_Arrow_Up.svg.png" 
+             style="width: 10rem; height: 10rem;" 
+        />
+      </div>
+      <div id="l6" class="ba rotate-270" style="width: 10rem; height: 10rem; margin: 4em;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Black_Arrow_Up.svg/2000px-Black_Arrow_Up.svg.png" 
+             style="width: 10rem; height: 10rem;" 
+        />
+      </div>
+      <div id="l7" class="ba rotate-315" style="width: 10rem; height: 10rem; margin: 4em;">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Black_Arrow_Up.svg/2000px-Black_Arrow_Up.svg.png" 
+             style="width: 10rem; height: 10rem;" 
+        />
+      </div>
+    </div>
+  </div>
+</template>
+ 
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
+
+<script>
+export default {
+  data () {
+    return {}
+  },
+  // methods: {
+  //   captureClick () {
+  //     console.log(this.$refs)
+  //   }
+  // }
+  mounted () {
+    console.log(window.getComputedStyle("l1", "transform"))
+  }
+}
+</script>
