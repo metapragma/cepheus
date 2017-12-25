febd6ffc3d99eca168061573ece85199d32d850c
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-25 18:12:49 +0400

    add: heights and images

---
+++ src/components/partials-heights.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ba h1" style="width: 10rem; margin: 4em;"></div>
+      <div id="l2" class="ba h2" style="width: 10rem; margin: 4em;"></div>
+      <div id="l3" class="ba h3" style="width: 10rem; margin: 4em;"></div>
+      <div id="l4" class="ba h4" style="width: 10rem; margin: 4em;"></div>
+      <div id="l5" class="ba h5" style="width: 10rem; margin: 4em;"></div>
+      <div class="ba h5" style="width: 10rem; height: 50rem; margin: 4em;">
+        <div id="l6" class="ba h-25" style="width: 10rem;"></div>
+      </div>
+      <div class="ba h5" style="width: 10rem; height: 50rem; margin: 4em;">
+        <div id="l7" class="ba h-50" style="width: 10rem;"></div>
+      </div>
+      <div class="ba h5" style="width: 10rem; height: 50rem; margin: 4em;">
+        <div id="l8" class="ba h-75" style="width: 10rem;"></div>
+      </div>
+      <div class="ba h5" style="width: 10rem; height: 50rem; margin: 4em;">
+        <div id="l9" class="ba h-100" style="width: 10rem;"></div>
+      </div>
+      <div class="ba h5" style="width: 10rem; height: 50rem; margin: 4em;">
+        <div id="l10" class="ba min-h-100" style="width: 10rem;"></div>
+      </div>
+      <div id="l11" class="ba vh-25" style="width: 10rem; margin: 4em;"></div>
+      <div id="l12" class="ba vh-50" style="width: 10rem; margin: 4em;"></div>
+      <div id="l13" class="ba vh-75" style="width: 10rem; margin: 4em;"></div>
+      <div id="l14" class="ba vh-100" style="width: 10rem; margin: 4em;"></div>
+      <div id="l15" class="ba min-vh-100" style="width: 10rem; margin: 4em;"></div>
+      <div id="l16" class="ba h-auto" style="width: 10rem; margin: 4em;"></div>
+      <div class="ba h5" style="width: 10rem; height: 50rem; margin: 4em;">
+        <div id="l17" class="ba h-inherit" style="width: 10rem;"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
