c8d201b2109bd28d3780968cf0b88b0ed7248695
Robert Adamyan <robert.adamyan1@gmail.com>
2017-12-27 17:48:59 +0400

    add: hovers

---
+++ src/components/partials-hovers.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ba bg-black dim" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l2" class="ba bg-black glow" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l3" class="ba bg-black hide-child" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l4" class="ba bg-black underline-hover" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l5" class="ba bg-black grow" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l6" class="ba bg-black grow-large" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l7" class="ba bg-black pointer" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l8" class="ba bg-black shadow-hover" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+      <div id="l9" class="ba bg-black bg-animate" style="width: 10rem; height: 10rem; margin: 4em;"></div>
+    </div>
+  </div>
+</template>
+ 
+<style lang="scss">
+@import '../styles/entrypoints/partials';
+</style>
