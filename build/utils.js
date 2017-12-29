d09fe35cba766e8c3db155682545e1fc98a89c23
MM <mark@epiloque.com>
2017-12-29 11:20:32 +0000

    further build cleanup
    Signed-off-by: MM <mark@epiloque.com>

--- build/utils.js
+++ build/utils.js
@@ -1,14 +1,10 @@
 'use strict'
 
 const path = require('path')
-const config = require('../config')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 
 exports.assetsPath = function (_path) {
-  const assetsSubDirectory = process.env.NODE_ENV === 'production'
-    ? config.build.assetsSubDirectory
-    : config.dev.assetsSubDirectory
-  return path.posix.join(assetsSubDirectory, _path)
+  return path.posix.join('src', _path)
 }
 
 exports.cssLoaders = function (options) {
