f8ceb649712579872583711ad81273bd4e249c1f
Robert Adamyan <robert.adamyan1@gmail.com>
2018-01-03 17:00:23 +0400

    add: spacing

---
+++ src/components/partials-spacing.vue
@@ -0,0 +1,306 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <!-- margin -->
+      <div>
+        <div id="l1" class="ba dib ma0" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l2" class="ba dib ma1" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l3" class="ba dib ma2" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l4" class="ba dib ma3" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l5" class="ba dib ma4" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l6" class="ba dib ma5" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l7" class="ba dib ma6" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l8" class="ba dib ma7" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+
+      <!-- margin-left -->
+      <div>
+        <div id="l9" class="ba dib ml0" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l10" class="ba dib ml1" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l11" class="ba dib ml2" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l12" class="ba dib ml3" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l13" class="ba dib ml4" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l14" class="ba dib ml5" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l15" class="ba dib ml6" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l16" class="ba dib ml7" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+
+      <!-- margin-right -->
+      <div>
+        <div id="l17" class="ba dib mr0" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l18" class="ba dib mr1" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l19" class="ba dib mr2" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l20" class="ba dib mr3" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l21" class="ba dib mr4" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l22" class="ba dib mr5" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l23" class="ba dib mr6" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l24" class="ba dib mr7" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+
+    <!-- margin-bottom -->
+      <div>
+        <div id="l33" class="ba dib mb0" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l34" class="ba dib mb1" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l35" class="ba dib mb2" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l36" class="ba dib mb3" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l37" class="ba dib mb4" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l38" class="ba dib mb5" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l39" class="ba dib mb6" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l40" class="ba dib mb7" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+
+      <!-- margin-top -->
+      <div>
+        <div id="l41" class="ba dib mt0" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l42" class="ba dib mt1" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l43" class="ba dib mt2" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l44" class="ba dib mt3" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l45" class="ba dib mt4" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l46" class="ba dib mt5" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l47" class="ba dib mt6" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+      <div>
+        <div id="l48" class="ba dib mt7" style="width: 10rem; height: 10rem;"></div>
+        <div class="ba dib" style="width: 10rem; height: 10rem; margin: 2rem"></div>
+      </div>
+
+      <!-- padding -->
+      <div id="l49" class="ba pa0 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l50" class="ba pa1 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l51" class="ba pa2 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l52" class="ba pa3 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l53" class="ba pa4 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l54" class="ba pa5 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l55" class="ba pa6 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l56" class="ba pa7 mv4">
+        Lorem Ipsum
+      </div>
+
+      <!-- padding-left -->
+      <div id="l57" class="ba pl0 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l58" class="ba pl1 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l59" class="ba pl2 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l60" class="ba pl3 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l61" class="ba pl4 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l62" class="ba pl5 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l63" class="ba pl6 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l64" class="ba pl7 mv4">
+        Lorem Ipsum
+      </div>
+
+      <!-- padding-right -->
+      <div id="l65" class="ba pr0 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l66" class="ba pr1 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l67" class="ba pr2 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l68" class="ba pr3 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l69" class="ba pr4 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l70" class="ba pr5 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l71" class="ba pr6 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l72" class="ba pr7 mv47">
+        Lorem Ipsum
+      </div>
+
+      <!-- padding-top -->
+      <div id="l73" class="ba pt0 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l74" class="ba pt1 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l75" class="ba pt2 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l76" class="ba pt3 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l77" class="ba pt4 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l78" class="pba pt5 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l79" class="ba pt6 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l80" class="ba pt7 mv4">
+        Lorem Ipsum
+      </div>
+
+      <!-- padding-bottom -->
+      <div id="l81" class="ba pb0 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l82" class="ba pb1 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l83" class="ba pb2 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l84" class="ba pb3 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l85" class="ba pb4 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l86" class="ba pb5 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l87" class="ba pb6 mv4">
+        Lorem Ipsum
+      </div>
+      <div id="l88" class="ba pb7 mv4">
+        Lorem Ipsum
+      </div>
+
+    </div>
+  </div>
+</template>
