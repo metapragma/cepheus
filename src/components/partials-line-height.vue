356584453691098fbbeec940101e25c588aaffdd
Robert Adamyan <robert.adamyan1@gmail.com>
2018-01-03 15:13:58 +0400

    add: line-height tests

---
+++ src/components/partials-line-height.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="tests" style="margin: 5em;">
+    <div id="case-one">
+      <div id="l1" class="ba lh-solid" style="width: 30rem; height: 30rem; margin: 4em;">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit. 
+        <div id="l2" class="ba lh-title" style="width: 25rem; height: 25rem padding: 2rem;">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+          labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+          nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit. 
+          <div id="l3" class="ba" style="width: 15rem; height: 15rem padding: 1rem;">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+            labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit. 
+          </div>
+        </div>
+      </div>
+      <div id="l4" class="ba lh-copy" style="width: 20rem; height: 20rem; margin: 4em;">
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
+        labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
+        nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit. 
+      </div>
+    </div>
+  </div>
+</template>
+
