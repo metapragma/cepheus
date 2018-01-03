b03d1330827e66c682da17a4fc1d230b31f5e7ba
l5x <189198+l5x@users.noreply.github.com>
2018-01-03 07:55:27 +0000

    chore: pass tests

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: 'epiloque',
   rules: {
-    'import/no-extraneous-dependencies': ['error', {'devDependencies': ['build/**/*.js', 'test/**/*.js']}]
+    'import/no-extraneous-dependencies': ['error', {'devDependencies': ['build/**/*.js', 'test/**/*.js', 'src/**/*.js']}]
   }
 }
