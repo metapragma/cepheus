c368cc7b73ed6f7635d5c64537831fb24f9916c9
l5x <189198+l5x@users.noreply.github.com>
2018-01-09 16:40:37 +0000

    chore: /cepheus-register-partial/cepheus-register/

--- test/end-to-end/nightwatch.conf.js
+++ test/end-to-end/nightwatch.conf.js
@@ -33,7 +33,7 @@ module.exports = {
         javascriptEnabled: true,
         acceptSslCerts: true,
         chromeOptions: {
-          args: ['--headless']
+          args: ['headless', 'no-sandbox', 'disable-gpu']
         }
       }
     },
